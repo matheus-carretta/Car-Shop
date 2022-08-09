c2fb1ab007afe59293c94be13cebf37292cd3fa7
matheuscarretta <matheuscarretta@gmail.com>
2022-08-09 14:34:26 -0300

    feat: cria a service Car

---
+++ src/services/Cars.ts
@@ -0,0 +1,16 @@
+import IService from '../interfaces/IService';
+import { ICar } from '../interfaces/ICar';
+import { IModel } from '../interfaces/IModel';
+
+class CarService implements IService<ICar> { 
+  private _car:IModel<ICar>;
+  constructor(model:IModel<ICar>) {
+    this._car = model;
+  }
+
+  public async create(obj:ICar):Promise<ICar> {
+    return this._car.create(obj);
+  }
+}
+
+export default CarService;
