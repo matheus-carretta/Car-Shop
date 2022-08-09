9424e2eda0128c9421dbab130556a1b05956433c
matheuscarretta <matheuscarretta@gmail.com>
2022-08-09 14:25:15 -0300

    feat: cria a model de Cars

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import app from './app';
-import connectToDatabase from './models/Connection';
+import connectToDatabase from './connection';
 
 const PORT = process.env.PORT || 3001;
 connectToDatabase()
