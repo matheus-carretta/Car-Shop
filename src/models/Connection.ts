0cdeeecfd9c209aaa29bec6ce36b6e0d27794c1f
matheuscarretta <matheuscarretta@gmail.com>
2022-08-08 21:06:23 -0300

    feat: cria arquivo de conexao com o banco de dados

---
+++ src/models/Connection.ts
@@ -0,0 +1,9 @@
+import 'dotenv/config';
+import mongoose from 'mongoose';
+
+const connectToDatabase = (
+  mongoDatabaseURI = process.env.MONGO_URI
+    || 'mongodb://localhost:27017/',
+) => mongoose.connect(mongoDatabaseURI);
+
+export default connectToDatabase;
